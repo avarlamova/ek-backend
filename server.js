3dfd41f6a378cd0584a32d51080710bfe5b03c52
avarlamova <59831804+avarlamova@users.noreply.github.com>
2023-04-30 17:18:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const express = require("express");
 const cors = require("cors");
-const PORT = 3001 || process.env.PORT;
+// const PORT = 3001 || process.env.PORT;
 
 const app = express();
 app.use(cors());
@@ -20,27 +20,8 @@ app.get("/getTitle", (req, res) => {
     .catch((err) => {
       console.log(err);
     });
-  //   const website = req.params.website;
-  //   axios
-  //     .get(website)
-  //     .then((response) => {
-  //       const html = response.data;
-  //       const parser = new DOMParser();
-  //       const htmlDoc = parser.parseFromString(html, "text/html");
-  //       const title = htmlDoc.querySelector("title").textContent;
-  //       res.send(title);
-  //       console.log(title);
-  //     })
-  //     .catch((error) => {
-  //       console.error(error);
-  //       res.sendStatus(500);
-  //     });
 });
 
-app.listen(PORT, () => {
-  console.log("listening");
-});
-
-//
-// 2. получать website из параметров
-// 3.
+// app.listen(PORT, () => {
+//   console.log("listening");
+// });
